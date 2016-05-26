9f20d4d77d1caf4a0c74b96ebff6c0b148b2210e
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-05-26 08:58:13 -0400

    Minor updateds

--- play.rb
+++ play.rb
@@ -40,19 +40,27 @@ until game_over
     end
     puts "Dealer hand:"
     puts dealer.hand.to_s
-    #binding.pry
-    puts player.hand.beats? dealer.hand
-    puts
+
+    if player.hand.beats? dealer.hand
+      "Player hand beats"
+      round_over = true
+    end
     #binding.pry
     if dealer.hand.blackjack?
       puts "Dealer hit blakjack!"
     elsif player.hand.blackjack?
       puts "Player hit blackjack"
     end
-    if dealer.hand.busted? || player.hand.busted?
-      puts "Round over!"
+
+    if dealer.hand.busted?
+      puts "Dealer hand busted!"
+      round_over = true
+    elsif player.hand.busted?
+      puts "Player hand busted!"
       round_over = true
     end
+
+  
 
     # bust, round overp
   end
